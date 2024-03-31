b48f402e10a97fd182a9c105d73c41902c28c83d
Phoebelalala <let59@student.douglascollege.ca>
2024-03-31 07:50:09 -0700

    Implementation of Search View for Favourite Fragment and List Detail Fragment

--- app/src/main/java/com/example/save4fun/fragment/ListDetailFragment.java
+++ app/src/main/java/com/example/save4fun/fragment/ListDetailFragment.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.example.save4fun.R;
@@ -22,12 +23,16 @@ import java.util.List;
 
 public class ListDetailFragment extends Fragment {
 
-    List<Product> products = new ArrayList<>();
+    List<Product> productsInList = new ArrayList<>();
 
     TextView textViewProductsByList, textViewTotalPrice;
+
+    SearchView searchViewProductsInList;
+
     RecyclerView recyclerViewProductsInList;
 
     DBProductsHelper dbProductsHelper;
+    int listId = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -37,28 +42,44 @@ public class ListDetailFragment extends Fragment {
         textViewProductsByList = view.findViewById(R.id.textViewProductsByList);
         textViewTotalPrice = view.findViewById(R.id.textViewTotalPrice);
 
+        searchViewProductsInList = view.findViewById(R.id.searchViewProductsInList);
+
         recyclerViewProductsInList = view.findViewById(R.id.recyclerViewProductsInList);
 
         dbProductsHelper = new DBProductsHelper(getContext());
 
         Bundle listData = getArguments();
         if (listData != null) {
-            int listId = listData.getInt("listId");
+            listId = listData.getInt("listId");
             String listName = listData.getString("listName");
 
-            products = dbProductsHelper.getProductsByListId(listId);
+            productsInList = dbProductsHelper.getProductsByListId(listId);
             textViewProductsByList.setText("Products in " + listName);
 
-            double total = calculateTotalPrice(products);
+            double total = calculateTotalPrice(productsInList);
             DecimalFormat df = new DecimalFormat("$0.00");
             textViewTotalPrice.setText(df.format(total));
         }
 
-        ProductByListAdapter productByListAdapter = new ProductByListAdapter(products);
+        ProductByListAdapter productByListAdapter = new ProductByListAdapter(productsInList);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
 
         recyclerViewProductsInList.setAdapter(productByListAdapter);
         recyclerViewProductsInList.setLayoutManager(linearLayoutManager);
+
+        searchViewProductsInList.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                List<Product> filteredProductsInList = filterListOfProductsInList(newText);
+                productByListAdapter.updateData(filteredProductsInList);
+                return false;
+            }
+        });
 
         return view;
     }
@@ -69,5 +90,17 @@ public class ListDetailFragment extends Fragment {
             total += product.getPrice() * product.getQuantity();
         }
         return total;
+    }
+
+    private List<Product> filterListOfProductsInList(String text) {
+        productsInList = dbProductsHelper.getProductsByListId(listId);
+        List<Product> filteredProductsInList = new ArrayList<>();
+        for (Product productInList : productsInList) {
+            if (productInList.getName().toLowerCase().contains(text.toLowerCase())) {
+                filteredProductsInList.add(productInList);
+            }
+        }
+        productsInList = filteredProductsInList;
+        return filteredProductsInList;
     }
 }
